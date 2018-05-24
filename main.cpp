6595b0a977f3081b0385a690f97cd491ca0ca4d6
leslie <leslie@leslie.com>
2018-05-24 05:37:25 +0000

    add over light camera

--- main.cpp
+++ main.cpp
@@ -40,34 +40,138 @@
 //
 //M*/
 #include <opencv2/core/utility.hpp>
-#include <opencv2/videoio.hpp>
+#include <opencv2/imgproc.hpp>
 #include <opencv2/highgui.hpp>
 #include <iostream>
 #include <cstring>
+#include <sstream>
+#include <pthread.h>
+#include <stdlib.h>
+#include <stdio.h>   /* Standard input/output definitions */
+#include <string.h>  /* String function definitions */
+#include <unistd.h>  /* UNIX standard function definitions */
+#include <fcntl.h>   /* File control definitions */
+#include <errno.h>   /* Error number definitions */
+#include <termios.h> /* POSIX terminal control definitions */
 
-
+#include <linux/i2c-dev.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/ioctl.h>
 
 using namespace std;
 using namespace cv;
 
-
+namespace patch
+{
+    template < typename T > std::string to_string( const T& n )
+    {
+        std::ostringstream stm ;
+        stm << n ;
+        return stm.str() ;
+    }
+}
+std::string get_tegra_pipeline(int width, int height, int fps) {
+    return "nvcamerasrc sensor-id= 0 ! video/x-raw(memory:NVMM), width=(int)" + patch::to_string(width) + ", height=(int)" +
+           patch::to_string(height) + ", format=(string)I420, framerate=(fraction)" + patch::to_string(fps) +
+           "/1 ! nvvidconv flip-method=2 ! video/x-raw, format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)BGR ! appsink";
+           //"/1 ! nvvidconv flip-method=2 !  appsink";
+}
+void focal_length(unsigned short length){
+	unsigned short _length = length;
+	if(_length>993||length<0){
+		printf("error:focal_length:%d over flow\n",_length);
+	}else{
+		int file;
+		int adapter_nr = 2; //probably dynamically determined
+		char filename[20];
+		snprintf(filename,19,"/dev/i2c-%d",adapter_nr);
+		file = open(filename,O_RDWR);
+		if(file<0){
+			printf("open file failed\n");
+			exit(1);
+		}
+		int addr = 0x0c; //the i2c address
+		if(ioctl(file,I2C_SLAVE,addr)<0){
+			printf("ioctl error\n");
+			exit(1);
+		}
+		__u8 reg = 0x04;
+		__s32 res;
+		char buff[10];
+		buff[0] = reg;
+		memcpy(&buff[1],((unsigned char*)&_length)+1,1);
+		memcpy(&buff[2],((unsigned char*)&_length),1);
+		res = write(file,buff,3);
+		if(res!=3){
+			printf("write failed:%d\n",res);
+		}else{
+			}
+		}
+}
 int main(){
-	VideoCapture cap(0);
-	//cap.set(CAP_PROP_FRAME_WIDTH,240);
-	//cap.set(CAP_PROP_FRAME_HEIGHT,160);
-	if (!cap.isOpened()) {
+	int FPS = 30;
+	std::string pipeline = get_tegra_pipeline(1280, 720, FPS);
+	VideoCapture light_cap(pipeline, cv::CAP_GSTREAMER);
+	
+	VideoCapture heat_cap(1);
+	heat_cap.set(CAP_PROP_FRAME_WIDTH,320);
+	heat_cap.set(CAP_PROP_FRAME_HEIGHT,240);
+	if (!heat_cap.isOpened()||!light_cap.isOpened()) {
 		printf("cant open camera!\n");
 	} else {
-		for(;;)
+		Mat heat_img,light_img;
+		unsigned short length = 350;
+		focal_length(length);
+		char char_key = 'a';
+		
+		const char heat_window[] = "heatwindow";
+		const char light_window[] = "lightwindow";
+		namedWindow(heat_window,0);
+		namedWindow(light_window,1);
+		bool whileflag = true;
+		while(whileflag)
 		{
-			Mat frame;
-			cap >> frame;
-			imshow("frame", frame);
-			char cmd = waitKey(1);
-			if (cmd == 'b')
-				break;
-			if (cmd == 'f'){
-				cap.set(CAP_PROP_FOCUS,1);
+
+			heat_cap >> heat_img;
+			light_cap >> light_img;
+			Rect roi_rect = Rect(360+80,240,320,240);
+			Mat roi_img = light_img(roi_rect);
+			vector<Mat> rgb_img(3),rgb_binary_img(3);
+			split(heat_img,rgb_img);
+			//Canny( rgb_img[0], rgb_binary_img[0], 30, 30*3, 3 );
+			//Canny( rgb_img[1], rgb_binary_img[1], 30, 30*3, 3 );
+			//Canny( rgb_img[2], rgb_binary_img[2], 30, 30*3, 3 );
+			threshold( rgb_img[0], rgb_binary_img[0], 200, 255,3 );
+			threshold( rgb_img[1], rgb_binary_img[1], 200, 255,3 );
+			threshold( rgb_img[2], rgb_binary_img[2], 200, 255,3 );
+			merge(rgb_binary_img,heat_img);
+			Mat mixed_img;
+			addWeighted( roi_img, 1, heat_img, 1, 0.0, mixed_img);
+			mixed_img.copyTo(light_img(roi_rect));
+            
+			imshow(light_window, light_img);
+			
+
+			char_key = waitKey(1);
+			switch(char_key){
+			case 'f':length = length+10;
+			         if((length<993)&&(length>0)){
+						 focal_length(length);
+					 }else{
+						length = 0;
+					 }
+					break;
+			case 'g':length = length-10;
+			         if((length<993)&&(length>0)){
+						 focal_length(length);
+					 }else{
+						length = 0;
+					 }
+					break;
+			case 'c':imwrite("heat_img.bmp",heat_img);break;
+			case 'e':whileflag = false;break;
+			default:break;
 			}
 		}
 	}
